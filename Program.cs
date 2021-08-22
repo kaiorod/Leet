0fa011a344bcee8be5e7353cdb7e4ab818ec6eca
kaiorod <caiorodrigo2775@gmail.com>
2021-08-22 17:43:06 -0300

    finaliza Leet

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.Beep();
+            Console.Clear();
+            Console.WriteLine("Olá usuário, irei traduzir sua mensagem para Leet");
+            Console.Write("\nDigite a mensagem a ser traduzida : ");
+            texto = Console.ReadLine();
+
+            texto = texto.Replace("a" , "4")
+            .Replace("a" , "4")
+            .Replace("A" , "4")
+            .Replace("e" , "3")
+            .Replace("E" , "3")
+            .Replace("i" , "1")
+            .Replace("I" , "1")
+            .Replace("l" , "1")
+            .Replace("L" , "1")
+            .Replace("o" , "0")
+            .Replace("O" , "0")
+            .Replace("t" , "7")
+            .Replace("T" , "7")
+            .Replace("s" , "5")
+            .Replace("S" , "5");
+
+            Console.Beep();
+            Console.Beep();
+            Console.WriteLine("\nSua mensagem traduzida em Leet é : ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(texto);
+            Console.ResetColor();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Leet
 Faça um tradutor simples para a linguagem Leet Speak.
+
+Para isso, faça as seguintes substituições:
+
+Entrada	Saída
+A ou a	4
+E ou e	3
+I ou i ou L ou l	1
+O ou o	0
+T ou t	7
+S ou s	5
+Exemplo:
+
+Texto a ser traduzido...: Hackers usavam leet antes de virar modinha.
+TRADUZIDO: H4ck3r5 u54v4m 1337 4n735 d3 v1r4r m0d1nh4.
